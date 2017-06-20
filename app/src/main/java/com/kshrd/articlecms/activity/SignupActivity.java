db76911689374b620ac9ead91e1dc807aaa6fdfc
PhanPirang <pirang_phan@yahoo.com>
2017-06-20 15:01:01 +0700

    Upload Image

---
+++ app/src/main/java/com/kshrd/articlecms/activity/SignupActivity.java
@@ -0,0 +1,137 @@
+package com.kshrd.articlecms.activity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.gson.JsonObject;
+import com.kshrd.articlecms.R;
+import com.kshrd.articlecms.webservice.ImageService;
+import com.kshrd.articlecms.webservice.ServiceGenerator;
+
+import java.io.File;
+import java.security.Permission;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+import dmax.dialog.SpotsDialog;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class SignupActivity extends AppCompatActivity {
+
+
+    private static final int OPEN_GALLERY = 1;
+    private static final int READ_EXTERNAL_STORAGE_CODE = 1;
+    private Unbinder unbinder;
+    private String imagePath;
+    private ImageService imageService;
+    private SpotsDialog dialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        unbinder = ButterKnife.bind(this);
+        imageService = ServiceGenerator.createService(ImageService.class);
+        dialog = new SpotsDialog(this, "Please wait...");
+    }
+
+    @OnClick(R.id.btnSubmit)
+    void onSubmitClicked(){
+
+        File file = new File(imagePath);
+        RequestBody requestBody = RequestBody.create(MediaType.parse("multipart/form-data"), file);
+        MultipartBody.Part body = MultipartBody.Part.createFormData("FILE", file.getName(), requestBody);
+
+
+        Call<JsonObject> uploadImage = imageService.uploadSingleImage(body);
+
+        dialog.show();
+        uploadImage.enqueue(new Callback<JsonObject>() {
+            @Override
+            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                if (response.isSuccessful()){
+                    dialog.dismiss();
+                    Toast.makeText(SignupActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+                t.printStackTrace();
+                dialog.dismiss();
+                Toast.makeText(SignupActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @OnClick(R.id.btnBrowse)
+    void onButtonBrowseClicked() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            int checkedPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+            String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+            if (checkedPermission == PackageManager.PERMISSION_DENIED) {
+                requestPermissions(permissions, READ_EXTERNAL_STORAGE_CODE);
+            } else {
+                showGallery();
+            }
+        } else {
+            showGallery();
+        }
+    }
+
+    void showGallery() {
+        Intent galleryIntent = new Intent(
+                Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        startActivityForResult(galleryIntent, OPEN_GALLERY);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == READ_EXTERNAL_STORAGE_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                showGallery();
+            } else {
+                Toast.makeText(this, "Please Grant Permisson", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Uri selectedImage = data.getData();
+        String[] pathColumn = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(selectedImage, pathColumn, null, null, null);
+        if (cursor.moveToFirst()){
+            int columnIndex = cursor.getColumnIndex(pathColumn[0]);
+            imagePath = cursor.getString(columnIndex);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
+    }
+}
---
+++ app/src/main/java/com/kshrd/articlecms/webservice/ImageService.java
@@ -0,0 +1,23 @@
+package com.kshrd.articlecms.webservice;
+
+import com.google.gson.JsonObject;
+
+import org.json.JSONObject;
+
+import okhttp3.MultipartBody;
+import retrofit2.Call;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
+/**
+ * Created by pirang on 6/20/17.
+ */
+
+public interface ImageService {
+
+    @Multipart
+    @POST("v1/api/uploadfile/single")
+    Call<JsonObject> uploadSingleImage(@Part MultipartBody.Part body);
+
+}
